9619c779a6aa3731a15601b923a905f4b1c72cf8
Yuval <YuvalBerkovichCS@gmail.com>
2025-01-13 20:42:48 +0200

    Fix deploy1

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/FootballTeamsInfo">
-    2222
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
